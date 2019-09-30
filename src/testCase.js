a2f9c7e885b8bb746ee990754a40f1ef24519a7d
helayel <gunnerofdoom34@gmail.com>
2019-09-30 10:08:42 +0200

    Add node_modules

--- src/testCase.js
+++ src/testCase.js
@@ -17,7 +17,18 @@ class testCase {
 
 	// TODO but not like this : https://www.monkeyuser.com/2017/todo/
 	getExecutions(){
-		return null;
+		var obj = JSON.parse(this.json);
+		if (undefined !== obj.executions) {
+			if (Array.isArray(obj.executions))
+			{
+				return obj.executions;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
 	}
 }
 
