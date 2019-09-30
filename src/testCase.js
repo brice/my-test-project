895d30847d4fbe2b31bcf23a09ae7ccd7eeca289
TestNRGT <test@test.test>
2019-09-30 11:08:29 +0200

    Ajout des tests et de la vérification de style dans .travis.yml

--- src/testCase.js
+++ src/testCase.js
@@ -1,3 +1,4 @@
+"use strict";
 class testCase {
 	constructor(json) {
 		this.json = json;
@@ -17,7 +18,11 @@ class testCase {
 
 	// TODO but not like this : https://www.monkeyuser.com/2017/todo/
 	getExecutions(){
-		return null;
+		var obj = JSON.parse(this.json);
+		if (undefined !== obj.executions && 				Array.isArray(obj.executions)) {
+			return obj.executions;
+		}
+		return false;
 	}
 }
 
